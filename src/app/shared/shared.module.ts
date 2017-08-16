2350871e96caa381f33f675173554349a53c05d2
Alexandre Henrique Kavalerski Teixeira <kavalerskialexandre@gmail.com>
2017-08-16 13:07:15 -0300

    Creating and aplying snackbar animation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,15 +8,17 @@ import { RatingComponent } from './rating/rating.component';
 
 import { OrderService } from '../order/order.service'
 import { RestaurantsService } from '../restaurants/restaurants.service'
-import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service'
+import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
+import { SnackbarComponent } from './messages/snackbar/snackbar.component'
 
 
 @NgModule({
-  declarations: [InputComponent, RadioComponent, RatingComponent],
+  declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
   imports: [ CommonModule,FormsModule, ReactiveFormsModule],
   exports: [
     InputComponent, RadioComponent, RatingComponent,
-    CommonModule, FormsModule, ReactiveFormsModule
+    CommonModule, FormsModule, ReactiveFormsModule,
+    SnackbarComponent
   ]
 
 })
